21b954abd1960981420a5140d65f71a87acfc6c5
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-05-16 15:51:00 -0300

    feat: layout refactor
    Adds styled components, changes the basic components and removes
    unnecessary rules from the layout.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,16 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import Profile from './profile';
+import './layout.css';
 
-import Header from "./header"
-import "./layout.css"
+const Layout = styled.div`
+  display: flex;
+`;
 
-const Layout = ({ children }) => {
+const Container = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -21,31 +19,18 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+    <Layout>
+      <Profile profileName='Eric Madureira' />
+      <main>{children}</main>
+    </Layout>
+  );
+};
 
-Layout.propTypes = {
+Container.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Container;
